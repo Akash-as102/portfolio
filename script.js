819168392ff65ea0b67f3b6af611f803f211751c
Akash A S <akashsumesh2004@gmail.com>
2024-01-06 15:59:09 +0530

    completed fully

--- script.js
+++ script.js
@@ -1,66 +1,116 @@
-document.addEventListener("DOMContentLoaded",()=>{
-    const loadingText=document.querySelectorAll(".appear");
-    let observer = new IntersectionObserver(function(entries) { 
-            entries.forEach(function(entry) {
-                if (entry.isIntersecting) {
-                    entry.target.classList.add('visible');
-                }
-            });
-        }, { threshold: 0.5 });
-        loadingText.forEach(function(section) {
-            observer.observe(section);
-        });
-})
-let currentSection="";
-function handleIntersection(entries, observer) {
-    entries.forEach(entry => {
-        if (entry.isIntersecting) {
-            currentSection=entry.target.id;
-        }
-        if(currentSection=="home"){
-            let currentTag=document.querySelector("#hometag").classList.add("active");
-            currentTag=document.querySelector("#abouttag").classList.remove("active");
-            currentTag=document.querySelector("#skillstag").classList.remove("active");
-            currentTag=document.querySelector("#educationtag").classList.remove("active");
-            currentTag=document.querySelector("#contacttag").classList.remove("active");
-        }
-        else if(currentSection=="about"){
-            let currentTag=document.querySelector("#abouttag").classList.add("active");
-            currentTag=document.querySelector("#hometag").classList.remove("active");
-            currentTag=document.querySelector("#skillstag").classList.remove("active");
-            currentTag=document.querySelector("#educationtag").classList.remove("active");
-            currentTag=document.querySelector("#contacttag").classList.remove("active");
-        }
-        else if(currentSection=="skills"){
-            let currentTag=document.querySelector("#skillstag").classList.add("active");
-            currentTag=document.querySelector("#hometag").classList.remove("active");
-            currentTag=document.querySelector("#abouttag").classList.remove("active");
-            currentTag=document.querySelector("#educationtag").classList.remove("active");
-            currentTag=document.querySelector("#contacttag").classList.remove("active");
-        }
-        else if(currentSection=="education"){
-            let currentTag=document.querySelector("#educationtag").classList.add("active");
-            currentTag=document.querySelector("#hometag").classList.remove("active");
-            currentTag=document.querySelector("#skillstag").classList.remove("active");
-            currentTag=document.querySelector("#abouttag").classList.remove("active");
-            currentTag=document.querySelector("#contacttag").classList.remove("active");
-        }
-        else if(currentSection=="contact"){
-            let currentTag=document.querySelector("#contacttag").classList.add("active");
-            currentTag=document.querySelector("#hometag").classList.remove("active");
-            currentTag=document.querySelector("#skillstag").classList.remove("active");
-            currentTag=document.querySelector("#educationtag").classList.remove("active");
-            currentTag=document.querySelector("#abouttag").classList.remove("active");
-        }
-    });
-}
-const options = {
-    root: null, 
-    rootMargin: '0px', 
-    threshold: 0.5 
-}
-const observer = new IntersectionObserver(handleIntersection, options);
+let homeSection=document.querySelector("#home");
+let skillsSection=document.querySelector("#skills")
+let aboutSection=document.querySelector("#about");
+let educationSection=document.querySelector("#education");
+let contactSection=document.querySelector("#contact");
+let tree=document.querySelector(".tree");
+let btnabout=document.querySelector(".btn")
 
-document.querySelectorAll('section').forEach(section => {
-    observer.observe(section);
-});
+// tag declaration
+let home=document.querySelector("#hometag");
+let about=document.querySelector("#abouttag");
+let skills=document.querySelector("#skillstag");
+let contact=document.querySelector("#contacttag");
+let education=document.querySelector("#educationtag");
+let footSection=document.querySelector(".foot");
+
+function previous(){
+    if(currentSection=="home"){
+        homeSection.classList.remove("active");
+        home.classList.remove("active");
+    }
+    else if(currentSection=="about"){
+        aboutSection.classList.remove("active");
+        about.classList.remove("active");
+    }
+    else if(currentSection=="skills"){
+        tree.classList.remove("active");
+        skills.classList.remove("active");
+    }
+    else if(currentSection=="education"){
+        educationSection.classList.remove("active");
+        education.classList.remove("active");
+    }
+    else if(currentSection=="contact"){
+        contactSection.classList.remove("active");
+        contact.classList.remove("active");
+        footSection.classList.remove("active");
+    }
+}
+
+let currentSection="home";
+
+document.addEventListener("DOMContentLoaded",()=>{
+    const loadingText=homeSection.querySelectorAll(".appear");
+    homeSection.classList.add("visible");
+    loadingText.forEach(function(section){
+        section.classList.add('visible');
+    });
+})
+about.onclick=()=>{
+    previous();
+    const loadingText=aboutSection.querySelectorAll(".appear");
+    aboutSection.classList.add("visible");
+    loadingText.forEach(function(section){
+        section.classList.add('visible');
+    });
+    aboutSection.classList.add("active");
+    about.classList.add("active");
+    currentSection="about";
+}
+btnabout.onclick=()=>{
+    previous();
+    const loadingText=aboutSection.querySelectorAll(".appear");
+    aboutSection.classList.add("visible");
+    loadingText.forEach(function(section){
+        section.classList.add('visible');
+    });
+    aboutSection.classList.add("active");
+    about.classList.add("active");
+    currentSection="about";
+}
+home.onclick=()=>{
+    previous();
+    const loadingText=homeSection.querySelectorAll(".appear");
+    homeSection.classList.add("visible");
+    loadingText.forEach(function(section){
+        section.classList.add('visible');
+    });
+    homeSection.classList.add("active");
+    home.classList.add("active");
+    currentSection="home";    
+}
+skills.onclick=()=>{
+    previous();
+    const loadingText=skillsSection.querySelectorAll(".appear");
+    skillsSection.classList.add("visible");
+    loadingText.forEach(function(section){
+        section.classList.add('visible');
+    });
+    tree.classList.add("active");
+    skills.classList.add("active");
+    currentSection="skills";    
+}
+education.onclick=()=>{
+    previous();
+    const loadingText=educationSection.querySelectorAll(".appear");
+    educationSection.classList.add("visible");
+    loadingText.forEach(function(section){
+        section.classList.add('visible');
+    });
+    educationSection.classList.add("active");
+    education.classList.add("active");
+    currentSection="education";    
+}
+contact.onclick=()=>{
+    previous();
+    const loadingText=contactSection.querySelectorAll(".appear");
+    contactSection.classList.add("visible");
+    loadingText.forEach(function(section){
+        section.classList.add('visible');
+    });
+    contactSection.classList.add("active");
+    footSection.classList.add("active")
+    contact.classList.add("active");
+    currentSection="contact";    
+}
